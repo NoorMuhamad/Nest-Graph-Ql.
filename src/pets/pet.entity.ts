c0bf80df71abadb8f471b50e258ec57ee3f0227b
NoorMuhamad <noorrasheed317@gmail.com>
2024-01-01 09:57:18 +0500

    initial commit

---
+++ src/pets/pet.entity.ts
@@ -0,0 +1,11 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class Pet {
+  @Field(() => Int)
+  id: number;
+  @Field()
+  name: string;
+  @Field({ nullable: true })
+  type?: string;
+}
