e2449a556053438e039436a490fa63d9b37e6ee5
NoorMuhammad <noorrasheed317@gmail.com>
2024-02-06 16:19:13 +0500

    minor fixes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000, https://admin-panel-phi-eight.vercel.app',
+    origin: 'https://admin-panel-phi-eight.vercel.app',
     credentials: true,
   });
   await app.listen(8080);
